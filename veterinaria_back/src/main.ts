f4da7f60c0d0d563bf26cc911ae131be1647789d
Josefer98 <alfa6547@gmail.com>
2025-10-30 12:29:37 -0400

    cofiguracion main ts

--- veterinaria_back/src/main.ts
+++ veterinaria_back/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  app.enableVersioning({type: VersioningType.URI, defaultVersion: '1'});
+  await app.listen(process.env.PORT ?? 0);
+  console.log(`Back de veterinaria corriendo en : ${await app.getUrl()}`);
 }
 bootstrap();
