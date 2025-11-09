55b8dc5f660f08938ac18e677f9a41908ff26870
Julio Cesar Escobar <julioos_97@hotmail.com>
2025-11-09 12:40:24 -0400

    Commit 2

--- veterinaria_back/src/facturaserivicios/facturaserivicios.module.ts
+++ veterinaria_back/src/facturaserivicios/facturaserivicios.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FacturaseriviciosService } from './facturaserivicios.service';
 import { FacturaseriviciosController } from './facturaserivicios.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Facturaserivicio } from './entities/facturaserivicio.entity';
 
 @Module({
+
+  imports: [
+    TypeOrmModule.forFeature([Facturaserivicio]),
+  ],
   controllers: [FacturaseriviciosController],
   providers: [FacturaseriviciosService],
 })
-export class FacturaseriviciosModule {}
+export class FacturaseriviciosModule { }
--- veterinaria_back/src/facturaserivicios/facturaserivicios.service.ts
+++ veterinaria_back/src/facturaserivicios/facturaserivicios.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateFacturaserivicioDto } from './dto/create-facturaserivicio.dto';
 import { UpdateFacturaserivicioDto } from './dto/update-facturaserivicio.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Facturaserivicio } from './entities/facturaserivicio.entity';
+import { Repository } from 'typeorm';
+
 
 @Injectable()
+
 export class FacturaseriviciosService {
-  create(createFacturaserivicioDto: CreateFacturaserivicioDto) {
-    return 'This action adds a new facturaserivicio';
+  constructor(
+    @InjectRepository(Facturaserivicio)
+    private facturaseriviciosRepository: Repository<Facturaserivicio>,
+  ) { }
+  async create(
+    createFacturaserivicioDto: CreateFacturaserivicioDto): Promise<Facturaserivicio> {
+    let facturaserivicio = await this.facturaseriviciosRepository.findOneBy({
+      idServicio: createFacturaserivicioDto.idServicio,
+      idVenta: createFacturaserivicioDto.idVenta,
+    });
+    if (facturaserivicio)
+      throw new ConflictException('El servicio ya existe en la factura');
+    facturaserivicio = new Facturaserivicio();
+    Object.assign(facturaserivicio, createFacturaserivicioDto);
+    return this.facturaseriviciosRepository.save(facturaserivicio);
   }
 
-  findAll() {
-    return `This action returns all facturaserivicios`;
+
+
+  async findAll(): Promise<Facturaserivicio[]> {
+    return this.facturaseriviciosRepository.find({
+      relations: { servicio: true, venta: true },
+      select: {
+        id: true,
+        cantidad: true,
+        total: true,
+        servicio: { id: true },
+        venta: { id: true },
+      },
+      order: { id: 'ASC' },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} facturaserivicio`;
+
+
+  async findOne(id: number): Promise<Facturaserivicio> {
+    const facturaserivicio = await this.facturaseriviciosRepository.findOne({
+      where: { id },
+      relations: { servicio: true, venta: true },
+    });
+    if (!facturaserivicio)
+      throw new ConflictException('Facturaserivicio no encontrado');
+    return facturaserivicio;
   }
 
-  update(id: number, updateFacturaserivicioDto: UpdateFacturaserivicioDto) {
-    return `This action updates a #${id} facturaserivicio`;
+  async update(id: number, updateFacturaserivicioDto: UpdateFacturaserivicioDto): Promise<Facturaserivicio> {
+    const facturaserivicio = await this.facturaseriviciosRepository.findOneBy({
+      id,
+    });
+    if (!facturaserivicio)
+      throw new NotFoundException('Facturaserivicio no encontrado');
+    Object.assign(facturaserivicio, updateFacturaserivicioDto);
+    return this.facturaseriviciosRepository.save(facturaserivicio);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} facturaserivicio`;
+  async remove(id: number): Promise<Facturaserivicio> {
+    const facturaserivicio = await this.findOne(id);
+    return this.facturaseriviciosRepository.softRemove(facturaserivicio);
   }
 }
